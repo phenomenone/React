55d7688df3012b0a6c4aa06821f53f6674fcec0e
boraxphenom <35803612+boraxphenom@users.noreply.github.com>
2021-06-02 15:27:19 -0300

    Adding cotizador files

---
+++ cotizador/src/components/Resumen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { primerMayuscula } from '../helper';
+import PropTypes from 'prop-types';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #fff;
+    margin-top: 1rem;
+`;
+
+const Resumen = ({datos}) => {
+
+    // extraer de datos
+    const { marca, year, plan } = datos;
+
+    if (marca === '' || year === ''|| plan === '' ) return null;
+
+    return (
+
+        <ContenedorResumen>
+
+            <h2>Resumen de Cotización</h2>
+            <ul>
+                <li>Marca: {primerMayuscula(marca)}</li>
+                <li>Plan: {primerMayuscula(plan)}</li>
+                <li>Año: {year}</li>
+            </ul>
+        </ContenedorResumen>
+        
+     );
+}
+
+Resumen.propTypes = {
+    datos: PropTypes.object.isRequired
+}
+export default Resumen;
