ff370d841df7fb9ee101a9c0588fe5756580b8a2
boraxphenom <35803612+boraxphenom@users.noreply.github.com>
2021-06-02 15:22:01 -0300

    Adding basicos project files

---
+++ basicos/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = ({fecha}) => {
+
+    return (
+        <footer>
+            <p>Todos los derechos reservador &copy; {fecha}</p>
+        </footer>
+    );
+}
+ 
+export default Footer;
