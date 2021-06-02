17bfa3ef1cfe0b7e3f50bbdbf4d165efe744ca5d
boraxphenom <35803612+boraxphenom@users.noreply.github.com>
2021-06-02 15:23:42 -0300

    adding breaking bad files

---
+++ breakingbad/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Frase from './components/Frase';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .6s ease;
+
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
+`;
+
+function App() {
+
+  // State frases
+  const[ frase, guardarFrase ]= useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://breakingbadapi.com/api/quotes');
+    const frase = await api.json()
+    guardarFrase(frase[0]);
+  }
+
+
+  // Cargar una fase
+  useEffect( () => {
+    consultarAPI()
+  }, []);
+
+  return (
+    <Contenedor>
+      <Frase
+        frase={frase}
+      />
+      <Boton
+        onClick={consultarAPI }
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
+  );
+}
+
+export default App;
